b86f7170bd16667d9c41745735647c2f97fbbf6b
sammi0715 <167386985+sammi0715@users.noreply.github.com>
2024-09-18 09:31:10 +0800

    Initialize Firebase

---
+++ src/utills/firebase.js
@@ -0,0 +1,21 @@
+
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, addDoc, setDoc, doc, query, orderBy, onSnapshot, serverTimestamp, getDocs, getDoc, where, deleteDoc, updateDoc } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+    apiKey: import.meta.env.VITE_APP_FIREBASE_API_KEY,
+    authDomain: "mood-flow.firebaseapp.com",
+    projectId: "mood-flow",
+    storageBucket: "mood-flow.appspot.com",
+    messagingSenderId: "394015348209",
+    appId: "1:394015348209:web:62d0ee52d026b7da78e050",
+    measurementId: "G-EXYMMZXCZF"
+};
+
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+export { db, auth, collection, addDoc, setDoc, doc, query, orderBy, onSnapshot, serverTimestamp, getDocs, getDoc, where, deleteDoc, updateDoc }
