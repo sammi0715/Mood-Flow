eff471c897df1e9f2cc0e392687bdf61a139bc85
sammi0715 <167386985+sammi0715@users.noreply.github.com>
2024-09-18 09:03:22 +0800

    Add route to diary-calendar/view-diary/callback pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import "./App.css";
 import Home from "./pages/Home";
 import DiaryCalendar from "./pages/Diary/DiaryCalendar";
 import NewDiaryEntry from "./pages/Diary/NewDiaryEntry";
@@ -9,13 +8,17 @@ import MoodTrack from "./pages/MoodTrack";
 import Settings from "./pages/Settings";
 import Header from "./pages/Header";
 import Footer from "./pages/Footer";
+import ViewDiaryEntry from "./pages/Diary/ViewDiaryEntry";
+import SpotifyCallback from "./pages/Diary/SpotifyCallback";
 function App() {
   return (
     <Router>
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/diary-calendar" element={<DiaryCalendar />} />
+        <Route path="/diary-calendar/:userId" element={<DiaryCalendar />} />
+        <Route path="/spotify-callback" element={<SpotifyCallback />} />
+        <Route path="/view-diary/:diaryId" element={<ViewDiaryEntry />} />
         <Route path="/new-diary-entry" element={<NewDiaryEntry />} />
         <Route path="/community" element={<Community />} />
         <Route path="/history-review" element={<HistoryReview />} />
