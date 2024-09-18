b5ba7dae43f5d40a97117b00c68c83f30ed1f358
sammi0715 <167386985+sammi0715@users.noreply.github.com>
2024-09-18 09:31:47 +0800

    Mood icon summary

---
+++ src/utills/moodIcons.js
@@ -0,0 +1,25 @@
+// src/utils/moodIcons.js
+
+import happy from "../assets/images/happy.png";
+import joy from "../assets/images/joy.png";
+import excited from "../assets/images/excited.png";
+import calm from "../assets/images/calm.png";
+import anxiety from "../assets/images/anxiety.png";
+import angry from "../assets/images/angry.png";
+import blue from "../assets/images/blue.png";
+import sad from "../assets/images/sad.png";
+import cry from "../assets/images/cry.png";
+
+const moodIcons = {
+    開心: happy,
+    快樂: joy,
+    興奮: excited,
+    平靜: calm,
+    焦慮: anxiety,
+    生氣: angry,
+    憂鬱: blue,
+    悲傷: sad,
+    哭泣: cry,
+};
+
+export default moodIcons;
