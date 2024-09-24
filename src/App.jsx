25cfd48d801ccb1b8f05875af79fae0db5ead4ad
sammi0715 <167386985+sammi0715@users.noreply.github.com>
2024-09-24 22:16:51 +0800

    Refactor component structure

--- src/App.jsx
+++ src/App.jsx
@@ -10,22 +10,32 @@ import Header from "./pages/Header";
 import Footer from "./pages/Footer";
 import ViewDiaryEntry from "./pages/Diary/ViewDiaryEntry";
 import SpotifyCallback from "./pages/Diary/SpotifyCallback";
+import { SpotifyPlayerProvider } from "./utills/SpotifyPlayerContext";
+
 function App() {
   return (
     <Router>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/diary-calendar/:userId" element={<DiaryCalendar />} />
-        <Route path="/spotify-callback" element={<SpotifyCallback />} />
-        <Route path="/view-diary/:diaryId" element={<ViewDiaryEntry />} />
-        <Route path="/new-diary-entry" element={<NewDiaryEntry />} />
-        <Route path="/community" element={<Community />} />
-        <Route path="/history-review" element={<HistoryReview />} />
-        <Route path="/mood-track" element={<MoodTrack />} />
-        <Route path="/settings" element={<Settings />} />
-      </Routes>
-      <Footer />
+      <SpotifyPlayerProvider>
+        <div className="flex flex-col min-h-screen">
+          <Header />
+
+          <main className="flex-1 overflow-auto">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/diary-calendar/:userId" element={<DiaryCalendar />} />
+              <Route path="/spotify-callback" element={<SpotifyCallback />} />
+              <Route path="/view-diary/:diaryId" element={<ViewDiaryEntry />} />
+              <Route path="/new-diary-entry" element={<NewDiaryEntry />} />
+              <Route path="/community/:userId" element={<Community />} />
+              <Route path="/history-review/:userId" element={<HistoryReview />} />
+              <Route path="/mood-track/:userId" element={<MoodTrack />} />
+              <Route path="/settings/:userId" element={<Settings />} />
+            </Routes>
+          </main>
+
+          <Footer />
+        </div>
+      </SpotifyPlayerProvider>
     </Router>
   );
 }
