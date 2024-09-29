8f5e50f75b444b85398d03ec877a7676f860b666
sammi0715 <167386985+sammi0715@users.noreply.github.com>
2024-09-29 10:49:29 +0800

    Update sidebar for responsive design

--- src/pages/Sidebar.jsx
+++ src/pages/Sidebar.jsx
@@ -45,50 +45,50 @@ function Sidebar({ isMenuOpen, setIsMenuOpen }) {
 
   return (
     <div
-      className={`fixed z-10 left-0 w-64 h-screen bg-stone-50 p-6 pl-10 transform transition-transform duration-300 ${
+      className={`absolute z-10 left-0 w-40 lg:w-64 h-screen bg-stone-50 p-4 lg:p-6 pl-4 lg:pl-10 transform transition-transform duration-300 overflow-y-hidden ${
         isMenuOpen ? "translate-x-0" : "-translate-x-full"
       }`}
     >
       {/* 右上角的 X 按鈕 */}
-      <div className="absolute top-4 right-4 cursor-pointer">
-        <IoClose size={24} onClick={() => setIsMenuOpen(false)} />
+      <div className="absolute top-1 right-2 cursor-pointer">
+        <IoClose className="w-[20px] h-[20px] lg:w-6 lg:h-6" onClick={() => setIsMenuOpen(false)} />
       </div>
 
       {/* 使用者頭像和名稱 */}
-      <div className="flex items-center mb-6">
+      <div className="flex items-center mb-4 lg:mb-6">
         <img
           src={profileImage || "https://via.placeholder.com/50"}
           alt="profile"
-          className="w-12 h-12 rounded-full mr-4"
+          className="w-8 h-8 lg:w-12 lg:h-12 rounded-full mr-2 lg:mr-4"
         />
         <div>
-          <h1 className="font-bold">{userName || "Username"}</h1>
+          <h1 className="font-bold text-sm">{userName || "Username"}</h1>
         </div>
       </div>
 
       {/* 側邊選單的項目 */}
       <ul>
         <li className="mb-4 flex items-center">
-          <FaUser className="mr-2 w-6 h-6" />
-          <Link to={`/community/${userId}`} className="text-lg">
+          <FaUser className="mr-2 w-4 h-4 lg:w-6 lg:h-6" />
+          <Link to={`/community/${userId}`} className="text-sm lg:text-lg">
             社群
           </Link>
         </li>
         <li className="mb-4 flex items-center">
-          <TiChartBar className="mr-2 w-6 h-6" />
-          <Link to={`/mood-track/${userId}`} className="text-lg">
+          <TiChartBar className="mr-2 w-4 h-4 lg:w-6 lg:h-6" />
+          <Link to={`/mood-track/${userId}`} className="text-sm lg:text-lg">
             心情統計
           </Link>
         </li>
         <li className="mb-4 flex items-center">
-          <FaHistory className="mr-2 w-6 h-6" />
-          <Link to={`/history-review/${userId}`} className="text-lg">
+          <FaHistory className="mr-2 w-4 h-4 lg:w-6 lg:h-6" />
+          <Link to={`/history-review/${userId}`} className="text-sm lg:text-lg">
             歷史回顧
           </Link>
         </li>
         <li className="mb-4 flex items-center">
-          <IoIosSettings className="mr-2 w-6 h-6" />
-          <Link to={`/settings/${userId}`} className="text-lg">
+          <IoIosSettings className="mr-2 w-4 h-4 lg:w-6 lg:h-6" />
+          <Link to={`/settings/${userId}`} className="text-sm lg:text-lg">
             設定
           </Link>
         </li>
@@ -98,7 +98,7 @@ function Sidebar({ isMenuOpen, setIsMenuOpen }) {
       <div className="absolute bottom-36 left-6 align-center">
         <button
           onClick={handleLogout}
-          className=" text-black px-4 py-2 rounded-md hover:bg-gray-200 transition flex items-center "
+          className="text-sm lg:text-base text-black lg:px-4 py-2 rounded-md hover:bg-gray-200 transition flex items-center "
         >
           <BiLogOutCircle className="mr-2" />
           登出
