2139230628cf8b95d4567e12c1f1c3ff59748a67
sammi0715 <167386985+sammi0715@users.noreply.github.com>
2024-09-29 10:39:09 +0800

    Update footer for responsive design

--- src/pages/Footer.jsx
+++ src/pages/Footer.jsx
@@ -1,12 +1,12 @@
 function Footer() {
   return (
-    <footer className="w-full h-12 bg-amber-100 flex items-center justify-center mt-10">
-      <div className="flex ">
-        <p className="mr-12">關於Mood Flow</p>
-        <p className="mr-12">服務條款</p>
-        <p className="mr-12">隱私政策</p>
-        <p className="mr-12">FAQ</p>
-        <p className="mr-12">聯絡我們</p>
+    <footer className="w-full h-12 sm:h-auto bg-amber-100 flex items-center justify-center z-10 p-2">
+      <div className="flex flex-wrap justify-center">
+        <p className="mr-2 xl:mr-12 sm:mr-4 sm:mb-0 text-sm xl:text-base">關於Mood Flow</p>
+        <p className="mr-2 xl:mr-12 sm:mr-4 sm:mb-0 text-sm xl:text-base">服務條款</p>
+        <p className="mr-2 xl:mr-12 sm:mr-4 sm:mb-0 text-sm xl:text-base">隱私政策</p>
+        <p className="mr-2 xl:mr-12 sm:mr-4 sm:mb-0 text-sm xl:text-base">FAQ</p>
+        <p className="mr-2 xl:mr-12 sm:mr-4 sm:mb-0 text-sm xl:text-base">聯絡我們</p>
       </div>
     </footer>
   );
