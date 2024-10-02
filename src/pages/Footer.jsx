aecf1b3493280a5177e2ddec5dfd3396173afc80
sammi0715 <167386985+sammi0715@users.noreply.github.com>
2024-10-02 17:23:29 +0800

    Fix footer in different page display

--- src/pages/Footer.jsx
+++ src/pages/Footer.jsx
@@ -1,6 +1,16 @@
+import { useLocation } from "react-router-dom";
+
 function Footer() {
+  const location = useLocation();
+
+  const isDiaryCalendar = location.pathname.startsWith("/diary-calendar");
+
   return (
-    <footer className="w-full h-12 sm:h-auto bg-amber-100 flex items-center justify-center z-10 p-2">
+    <footer
+      className={`w-full h-12 bg-amber-100 flex items-center justify-center ${
+        isDiaryCalendar ? "fixed bottom-0 left-0" : ""
+      } z-10`}
+    >
       <div className="flex flex-wrap justify-center">
         <p className="mr-2 xl:mr-12 sm:mr-4 sm:mb-0 text-sm xl:text-base">關於Mood Flow</p>
         <p className="mr-2 xl:mr-12 sm:mr-4 sm:mb-0 text-sm xl:text-base">服務條款</p>
@@ -11,4 +21,5 @@ function Footer() {
     </footer>
   );
 }
+
 export default Footer;
