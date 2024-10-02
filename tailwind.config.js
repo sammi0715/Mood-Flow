b75bd6cd7bf48988b5c8b76eb29f49b228ff8289
sammi0715 <167386985+sammi0715@users.noreply.github.com>
2024-10-02 17:15:36 +0800

    Add backgroundImage and colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,36 @@ export default {
     extend: {
       backgroundImage: {
         'mood-flow-banner': "url('/src/assets/images/mood-flow-banner.png')",
-      }
+        'mobile-mood-flow-banner': "url('/src/assets/images/mood-flow-mobile.png')",
+        'back': "url('/src/assets/images/grid.png')"
+      },
+      backgroundSize: {
+        'auto': 'auto',
+        'cover': 'cover',
+        'contain': 'contain',
+        '120-100': '120% 100%',
+        '130-100': '130% 100%',
+        '100-130': '100% 130%',
+      },
+      colors: {
+        'light-beige': '#fcf7eb',
+        'antique-white': ' #f7ead9',
+        'light-yellow': '#fcebaf',
+        'light-green': '#dfeeb7',
+        'dark-green': '#b6d367',
+        'light-orange': '#ffcc99',
+        'dark-orange': 'rgb(243 171 92)',
+        'pink-orange': '#efaea5',
+        'light-pink': '#e86868',
+        'light-blue': '#bbd5f3',
+        'light-blue-dark': '#9abee2',
+        'dark-blue': '#113d54',
+        'brown': 'rgb(230 220 205)',
+
+      },
+      fontFamily: {
+        'pencil-font': ['pencil-font', "sans-serif"],
+      },
     },
   },
   plugins: [],
