27aab89d649d29a708313555259719dded1fd9f7
sammi0715 <167386985+sammi0715@users.noreply.github.com>
2024-10-08 16:04:52 +0800

    Change alert style 、margin and font-size

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -4,6 +4,7 @@ import {
   updateUserData,
   handleImageUpload as firebaseHandleImageUpload,
 } from "../utills/firebase-data";
+import Alert from "../utills/alert";
 import { useParams } from "react-router-dom";
 import { FaCircleUser } from "react-icons/fa6";
 const ProfileSettings = () => {
@@ -18,7 +19,8 @@ const ProfileSettings = () => {
   const [privacyStatus, setPrivacyStatus] = useState("public");
   const [uploadedImages, setUploadedImages] = useState([]);
   const [previewImage, setPreviewImage] = useState(null);
-
+  const [alertMessage, setAlertMessage] = useState(null);
+  const [alertConfirm, setAlertConfirm] = useState(null);
   // 取得使用者資料
   useEffect(() => {
     const getUserData = async () => {
@@ -42,7 +44,7 @@ const ProfileSettings = () => {
       await firebaseHandleImageUpload(event, uploadedImages, setUploadedImages);
     } catch (error) {
       console.error("圖片上傳失敗：", error);
-      alert("圖片上傳失敗，請稍後再試。");
+      setAlertMessage("圖片上傳失敗，請稍後再試。");
     }
   };
 
@@ -65,18 +67,18 @@ const ProfileSettings = () => {
         updatedData.profile_pic = userData.profile_pic;
       }
       await updateUserData(userId, updatedData);
-      alert("個人資料更新成功！");
+      setAlertMessage("個人資料更新成功！");
     } catch (error) {
       console.error("更新個人資料失敗：", error);
-      alert("更新失敗，請稍後再試。");
+      setAlertMessage("更新失敗，請稍後再試。");
     }
   };
 
   return (
     <div className="min-h-screen p-8">
       <h2 className="text-2xl mb-4">個人資料設定</h2>
-      <div className="mb-4">
-        <label className="block text-gray-700 text-sm mb-2">頭像</label>
+      <div className="mb-4 md:ml-8">
+        <label className="block text-gray-700 text-sm md:text-lg mb-2">頭像</label>
         {uploadedImages.length > 0 ? (
           <img
             src={uploadedImages[0]}
@@ -97,27 +99,27 @@ const ProfileSettings = () => {
           className="py-2 px-3 mt-4"
         />
       </div>
-      <div className="mb-4">
-        <label className="block text-gray-700 text-sm mb-2">使用者名稱</label>
+      <div className="mb-4  md:ml-8">
+        <label className="block text-gray-700 text-sm md:text-lg mb-2">使用者名稱</label>
         <input
           type="text"
           value={newName}
           onChange={(e) => setNewName(e.target.value)}
-          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          className="shadow appearance-none border rounded w-[45%] py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
         />
       </div>
 
-      <div className="mb-4">
-        <label className="block text-gray-700 text-sm mb-2">隱私狀態</label>
+      <div className="mb-4  md:ml-8">
+        <label className="block text-gray-700 text-sm md:text-lg mb-2">隱私狀態</label>
         <select
           value={privacyStatus}
           onChange={(e) => setPrivacyStatus(e.target.value)}
-          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          className="shadow appearance-none border rounded w-[45%] py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
         >
           <option value="public">公開</option>
           <option value="private">私人</option>
         </select>
-        <p className="text-gray-500 text-xs mt-1">
+        <p className="text-gray-500 text-xs md:text-sm mt-1">
           <strong>公開：</strong>任何人都可以搜尋到您的帳戶並查看日記動態。
           <br />
           <strong>私人：</strong>只有您的好友可以查看日記動態。
@@ -126,10 +128,17 @@ const ProfileSettings = () => {
 
       <button
         onClick={handleUpdateProfile}
-        className="bg-dark-orange hover:bg-blue-400 text-white py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+        className="bg-dark-orange hover:bg-blue-400 text-white py-2 px-4 rounded focus:outline-none focus:shadow-outline md:ml-8"
       >
         更新資料
       </button>
+      {alertMessage && (
+        <Alert
+          message={alertMessage}
+          onClose={() => setAlertMessage(null)}
+          onConfirm={alertConfirm}
+        />
+      )}
     </div>
   );
 };
