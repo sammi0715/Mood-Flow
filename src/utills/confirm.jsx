ca4a51dee2a1d655cdc217a59b79fd5ed12c5e63
sammi0715 <167386985+sammi0715@users.noreply.github.com>
2024-10-12 17:51:53 +0800

    Fix comfirm alert css style

--- src/utills/confirm.jsx
+++ src/utills/confirm.jsx
@@ -3,15 +3,15 @@ import React from "react";
 function ConfirmDialog({ message, onConfirm, onCancel }) {
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
-      <div className="bg-white p-6 rounded-lg shadow-lg">
+      <div className="bg-white p-6 px-12 rounded-lg shadow-lg">
         <p className="text-lg mb-4">{message}</p>
-        <div className="flex justify-end space-x-4">
-          <button onClick={onCancel} className="px-4 py-2 bg-gray-300 rounded hover:bg-gray-400">
+        <div className="flex justify-center space-x-4 mt-8">
+          <button onClick={onCancel} className="px-2 py-2 bg-gray-300 rounded hover:bg-gray-400">
             取消
           </button>
           <button
             onClick={onConfirm}
-            className="px-4 py-2 bg-red-500 text-white rounded hover:bg-red-500"
+            className="px-2 py-2 bg-dark-orange text-white rounded hover:bg-amber-500"
           >
             確定
           </button>
