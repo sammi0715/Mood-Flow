7b3ec2708c26ba3fb2213b92e920dcc847a34780
sammi0715 <167386985+sammi0715@users.noreply.github.com>
2024-10-12 17:49:54 +0800

    Fix alert width

--- src/utills/alert.jsx
+++ src/utills/alert.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { IoClose } from "react-icons/io5";
 function CustomAlert({ message, onClose, onConfirm }) {
   return (
-    <div className="fixed top-5 left-1/2 transform -translate-x-1/2 justify-center w-[320px] p-4 bg-white opacity-95 text-black flex justify-between items-center shadow-lg z-50 rounded-lg">
+    <div className="fixed top-5 left-1/2 transform -translate-x-1/2 justify-center w-[340px] p-4 py-6 bg-white opacity-95 text-black flex justify-between items-center shadow-lg z-50 rounded-lg">
       <span>{message}</span>
       <button
         onClick={() => {
